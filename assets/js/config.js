c5ceca072131c652b3be76f2486ec1acc7ef3060
harussani manaphassan <harussani.suamisihat@gmail.com>
2026-09-10 17:25:55 +0800

    Enhance config UI: remove export & palette swatches, add dynamic social media manager with Telegram/TikTok/X, simplify 1-click auto-save

--- assets/js/config.js
+++ assets/js/config.js
@@ -121,8 +121,17 @@ const CONFIG = {
         youtube: {
             name: 'YouTube',
             url: 'https://www.youtube.com/@SuamiSihat',
-            iconPng: 'assets/img/social/youtube.png',
-            iconSvg: `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="#043388"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>`
+            iconPng: 'assets/img/social/youtube.png'
+        },
+        telegram: {
+            name: 'Telegram',
+            url: 'https://t.me/suamisihat',
+            iconPng: 'assets/img/social/telegram.png'
+        },
+        tiktok: {
+            name: 'TikTok',
+            url: 'https://www.tiktok.com/@suamisihat',
+            iconPng: 'assets/img/social/tiktok.png'
         }
     },
 
